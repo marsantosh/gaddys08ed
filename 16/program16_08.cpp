90e26f66ea0e5d8a5da6d7244274e3001100a3b9
mar <mar.dls@tutamail.com>
2020-04-30 18:43:02 -0500

    Use of function templates

---
+++ 16/program16_08.cpp
@@ -0,0 +1,33 @@
+// program16_08.cpp
+// this program illustrates the use of function templates
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+// template for minimum of an array
+template <class T>
+T minimum(T arr[], int size) {
+    T smallest = arr[0];
+    for (int k = 1; k < size; k++) {
+        if (arr[k] < smallest) {
+            smallest = arr[k];
+        }
+    }
+    return smallest;
+}
+
+
+int main() {
+    // the compiler creates int minimum(int [], int)
+    // when you pass an array of int
+    int arr1[] = { 40, 20, 35 };
+    cout << "The minimum number is " << minimum(arr1, 3) << endl;
+
+    // the compiler creates string minimum(string [], int)
+    // when you pass an array of string
+    string arr2[] = { "Zoe", "Snoopy", "Bob", "Waldorf" };
+    cout << "The minimum string is " << minimum(arr2, 4) << endl;
+
+    return 0;
+}
