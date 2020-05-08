9021330c60eec02bee0d31c825f430ace19272d5
mar <mar.dls@tutamail.com>
2020-05-07 22:44:28 -0500

    Adding remove() method to the NumberList class

--- 17/NumberList.cpp
+++ 17/NumberList.cpp
@@ -24,6 +24,43 @@ void NumberList::add(double number) {
 
 
 /*
+ * removes a nubmer from the list.
+ * the function does not assume that the list is sorted
+ */
+void NumberList::remove(double number) {
+    ListNode *nodePtr, *previousNodePtr;
+
+    // if the list is empty, do nothing
+    if (!head) {
+        return;
+    }
+
+    // determine if the first node is the one to delete
+    if (head->value == number) {
+        nodePtr = head;
+        head = head->next;
+        delete nodePtr;
+    }
+    else {
+        // initialize nodePtr to the head of the list
+        nodePtr = head;
+
+        // skip nodes whose value member is not number
+        while (nodePtr != NULL && nodePtr->value != number) {
+            previousNodePtr = nodePtr;
+            nodePtr = nodePtr->next;
+        }
+
+        // link the previous node to the node after
+        // nodePtr, then delete nodePtr
+        if (nodePtr) {
+            previousNodePtr->next = nodePtr->next;
+            delete nodePtr;
+        }
+    }
+}
+
+/*
  * `displayList` outputs a sequence of all values
  * currently sotred in the list
  */
