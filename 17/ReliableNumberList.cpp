2a81092c11ff348c3de2152107a05b42e4eed3a6
mar <mar.dls@tutamail.com>
2020-05-08 16:38:14 -0500

    Implementation of copy constructor and asignment operator overloading to NumberList. There is a bug here that I still cannot fix. I decided to continue with the book. Its on compile time.

---
+++ 17/ReliableNumberList.cpp
@@ -0,0 +1,54 @@
+#include "ReliableNumberList.h"
+
+//***************************************************
+// Copy constructor                                 *
+//***************************************************
+ReliableNumberList::
+ReliableNumberList(const ReliableNumberList& original)
+{
+    this->head = copyList(original.head);
+}
+
+//****************************************************
+// Overloaded Assignment operator                    *
+//****************************************************
+ReliableNumberList&
+ReliableNumberList::operator=(ReliableNumberList right)
+{
+    // First destroy the linked list in this object
+    destroyList(head);
+    // Assign a copy of the linked list in other object
+    head = copyList(right.head);
+}
+
+//****************************************************
+// Make a separate copy of the linked list inside    *
+// a ReliableNumberList object                       *
+//****************************************************
+NumberList::ListNode *
+ReliableNumberList::copyList(ListNode *aList)
+{
+    if (aList == NULL)
+        return NULL;
+    else
+    {
+        // First copy the tail
+        ListNode *tailCopy = copyList(aList->next);
+        // Return copy of head attached to copy of tail
+        return new ListNode(aList->value, tailCopy);
+    }
+}
+
+//******************************************************
+// Destroy a list by deallocating all of its nodes     *
+//******************************************************
+void ReliableNumberList::destroyList(ListNode *aList)
+{
+   if (aList != NULL)
+   {
+      ListNode *tail = aList->next;
+      // Deallocate the head and then destroy the tail
+      delete aList;
+      destroyList(tail);
+   }
+}
